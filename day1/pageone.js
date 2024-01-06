844c0183a49993a6c1e29d2dad3426fb0f8c242b
Adnan <abbasisami939@gmail.com>
2024-01-06 06:59:46 -0800

    add if else

---
+++ day1/pageone.js
@@ -0,0 +1,10 @@
+const age = 19;
+ if( age >=80){
+    console.log( " yes , you can vote");
+ }
+ else if( age >= 18){
+     console.log(" yes , you can !!");
+ } 
+ else{
+    console.log("no , yoc can't vote");
+ }
